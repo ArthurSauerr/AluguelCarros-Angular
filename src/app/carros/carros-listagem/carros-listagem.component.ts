5554a939fcb8522aa7d63b40c2d73e9e1023f4bd
ArthurSauerr <germanooarthuur@gmail.com>
2023-12-07 15:54:11 -0300

    Criado o componente de listagem de carro

---
+++ src/app/carros/carros-listagem/carros-listagem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carros-listagem',
+  standalone: true,
+  imports: [],
+  templateUrl: './carros-listagem.component.html',
+  styleUrl: './carros-listagem.component.scss'
+})
+export class CarrosListagemComponent {
+
+}
